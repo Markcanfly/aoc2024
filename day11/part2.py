18eb4e7950db2ea88e72d386d55c326c66f238bc
Márk Vince Varga <mark.vince.varga@gmail.com>
2024-12-12 16:34:42 +0100

    add blink result caching to optimize
    Since the blink result only depends on the input, not the iter count,
    separate it out to a distinct memoized (cached) function call.
    Improved performance by ~50% on input data.

--- day11/part2.py
+++ day11/part2.py
@@ -3,18 +3,25 @@ from functools import cache
 
 
 @cache
+def blink(number: int) -> int | tuple[int, int]:
+    if number == 0:
+        return 1
+    elif len(str_item := str(number)) % 2 == 0:
+        return int(str_item[: len(str_item) // 2]), int(str_item[len(str_item) // 2 :])
+    else:
+        return 2024 * number
+
+
+@cache
 def count_splits_into(number: int, after: int) -> int:
     """Find out how many numbers this will split into after an arbitrary number of iterations"""
     if after == 0:
         return 1
-    if number == 0:
-        return count_splits_into(1, after - 1)
-    elif len(str_item := str(number)) % 2 == 0:
-        return count_splits_into(
-            int(str_item[: len(str_item) // 2]), after - 1
-        ) + count_splits_into(int(str_item[len(str_item) // 2 :]), after - 1)
-    else:
-        return count_splits_into(2024 * number, after - 1)
+    match blink(number):
+        case (n1, n2):
+            return count_splits_into(n1, after - 1) + count_splits_into(n2, after - 1)
+        case next_number:
+            return count_splits_into(next_number, after - 1)
 
 
 def main():
