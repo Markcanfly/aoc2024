941324fe1a8b1cff4a11e9faf49b8c8086808af3
Márk Vince Varga <mark.vince.varga@gmail.com>
2024-12-13 17:10:18 +0100

    add day 13 solutions

---
+++ day13/part2.py
@@ -0,0 +1,57 @@
+import sys
+
+import numpy as np
+
+
+def extract_machine(raw_machine: str) -> tuple[np.ndarray, np.ndarray]:
+    parts = raw_machine.split("\n")
+    return (
+        np.array(
+            [
+                list(map(int, parts[0].removeprefix("Button A: X+").split(", Y+"))),
+                list(map(int, parts[1].removeprefix("Button B: X+").split(", Y+"))),
+            ]
+        ).transpose(),
+        np.array(
+            list(
+                map(
+                    lambda x: int(x) + 10000000000000,
+                    parts[2].removeprefix("Prize: X=").split(", Y="),
+                )
+            )
+        ),
+    )
+
+
+def is_round(number: np.float64) -> bool:
+    return np.abs(np.round(number) - number) <= 0.001
+
+
+def machine_cost(machine: tuple[np.ndarray, np.ndarray], a_cost, b_cost) -> int | None:
+    A, b = machine
+    x = np.linalg.solve(A, b)
+    if is_round(x[0]) and is_round(x[1]) and 0 <= x[0] and 0 <= x[1]:
+        return int(a_cost * np.round(x[0]) + b_cost * np.round(x[1]))
+    else:
+        return None
+
+
+def main():
+    if len(sys.argv) != 2:
+        raise ValueError("1 argument expected: inputfile")
+
+    with open(sys.argv[1]) as infile:
+        raw_machines = infile.read().split("\n\n")
+
+    machines = [extract_machine(machine) for machine in raw_machines]
+
+    total_cost = sum(
+        cost
+        for machine in machines
+        if (cost := machine_cost(machine, 3, 1)) is not None
+    )
+    print("Total cost of all viable machines:", total_cost)
+
+
+if __name__ == "__main__":
+    main()
