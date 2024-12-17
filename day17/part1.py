a97462757e44291c5faba0ccb2d980453b41660a
Márk Vince Varga <mark.vince.varga@gmail.com>
2024-12-17 16:33:40 +0100

    add day 17 part 1 solution

---
+++ day17/part1.py
@@ -0,0 +1,69 @@
+import sys
+from typing import Literal
+
+
+def combo(operand: int, registers: dict[Literal["A", "B", "C"], int]) -> int:
+    match operand:
+        case n if 0 <= n <= 3:
+            return n
+        case 4:
+            return registers["A"]
+        case 5:
+            return registers["B"]
+        case 6:
+            return registers["C"]
+        case 7:
+            raise NotImplementedError("combo operand 7 is reserved")
+
+
+def main():
+    if len(sys.argv) != 2:
+        raise ValueError("1 argument expected: inputfile")
+
+    with open(sys.argv[1]) as infile:
+        raw_registers, raw_program = infile.read().split("\n\n")
+        reg: dict[Literal["A", "B", "C"], int] = {
+            reg: int(raw_registers.split("\n")[idx].removeprefix(f"Register {reg}: "))
+            for idx, reg in enumerate(["A", "B", "C"])
+        }  # pyright: ignore
+
+        program = tuple(
+            map(int, raw_program.strip().removeprefix("Program: ").split(","))
+        )
+
+        operations = list(
+            (program[idx], program[idx + 1]) for idx in range(0, len(program), 2)
+        )
+
+    ptr = 0
+    print_buffer = []
+    while 0 <= ptr < len(operations):
+        jumped = False
+        opcode, operand = operations[ptr]
+        match opcode:
+            case 0:  # adv
+                reg["A"] = reg["A"] // (2 ** combo(operand, reg))
+            case 1:  # bxl
+                reg["B"] = reg["B"] ^ operand
+            case 2:  # bst
+                reg["B"] = combo(operand, reg) % 8
+            case 3:  # jnz
+                if reg["A"] != 0:
+                    jumped = True
+                    ptr = operand
+            case 4:  # bxc
+                reg["B"] = reg["B"] ^ reg["C"]
+            case 5:  # out
+                print_buffer.append(combo(operand, reg) % 8)
+            case 6:  # bdv
+                reg["B"] = reg["A"] // (2 ** combo(operand, reg))
+            case 7:  # cdv
+                reg["C"] = reg["A"] // (2 ** combo(operand, reg))
+        if not jumped:
+            ptr += 1
+
+    print(",".join(map(str, print_buffer)))
+
+
+if __name__ == "__main__":
+    main()
