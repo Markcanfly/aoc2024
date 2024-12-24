91ed875fb6b558b0192ead85acdaf3ebb4e27c8b
Márk Vince Varga <mark.vince.varga@gmail.com>
2024-12-24 14:20:45 +0100

    fix typo

--- day23/part1.py
+++ day23/part1.py
@@ -15,15 +15,15 @@ def main():
 
     nodes = set(node for edge in edges for node in edge)
 
-    clicks: list[tuple[str, str, str]] = []
+    cliques: list[tuple[str, str, str]] = []
     for three_nodes in combinations(nodes, r=3):
         if any(x.startswith("t") for x in three_nodes):
             if all(
                 (n1, n2) in edges for n1, n2 in combinations(sorted(three_nodes), r=2)
             ):
-                clicks.append(three_nodes)
+                cliques.append(three_nodes)
 
-    print("Number of clicks containing a node starting with t:", len(clicks))
+    print("Number of cliques containing a node starting with t:", len(cliques))
 
 
 if __name__ == "__main__":
